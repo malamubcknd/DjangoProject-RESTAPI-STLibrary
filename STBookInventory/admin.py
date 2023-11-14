810668a3b8e0837e1b39c9218c8b078e4b4b4d45
Marvin Alamu <marvinalamu_1@live.com>
2023-11-14 23:09:14 +0100

    made some changes in admin file

--- STBookInventory/admin.py
+++ STBookInventory/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 #from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
 
 from .models import User
@@ -9,4 +15,24 @@ from .models import BookCheckout
 admin.site.register(User)
 admin.site.register(Book)
 admin.site.register(BookCheckout)
-#admin.site.unregister(Group) #This unregisters the Groups menu from the admin site such that you can't see
+
+
+#I do not want staff to be able to change their status (staff, superuser, active) on the admin site
+#This unregisters the Groups menu from the admin site such that you can't see
+admin.site.unregister(User)
+#admin.site.unregister(Group)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+
+        if not is_superuser:
+            form.base_fields['is_active'].disabled = True
+            form.base_fields['is_superuser'].disabled = True
+            form.base_fields['email'].disabled = True
+            form.base_fields['user_permissions'].disabled = True
+            form.base_fields['groups'].disabled = True
+            form.base_fields['password'].disabled = True
+        return form
+
