2c1150941597602f1edd4411d243d286f0e4ced2
Marvin Alamu <marvinalamu_1@live.com>
2023-11-15 00:36:37 +0100

    implemented the field restrictions for staff member

--- STBookInventory/admin.py
+++ STBookInventory/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from django.contrib.auth import get_user_model
+from django.http.request import HttpRequest
 User = get_user_model()
 
 #from django.contrib.auth.models import Group
@@ -12,40 +13,45 @@ from .models import User
 from .models import Book
 from .models import BookCheckout
 
-# admin.site.register(User)
+#admin.site.register(User)
 admin.site.register(Book)
 admin.site.register(BookCheckout)
 
 
-#I do not want staff to be able to change their status (staff, superuser, active) on the admin site
-#This unregisters the Groups menu from the admin site such that you can't see
-# admin.site.unregister(User)
-#admin.site.unregister(Group)
-# @admin.register(User)
-# class CustomUserAdmin(UserAdmin):
-#     def get_form(self, request, obj=None, **kwargs):
-#         form = super().get_form(request, obj, **kwargs)
-#         is_superuser = request.user.is_superuser
-
-#         if not is_superuser:
-#             form.base_fields['is_active'].disabled = True
-#             form.base_fields['is_superuser'].disabled = True
-#             form.base_fields['email'].disabled = True
-#             form.base_fields['user_permissions'].disabled = True
-#             form.base_fields['groups'].disabled = True
-#             form.base_fields['password'].disabled = True
-#         return form
-
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    list_display = ("email",)
 
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
         is_superuser = request.user.is_superuser
 
         if not is_superuser:
-            form.base_fields[''].disable = True
+            form.base_fields['is_active'].disabled = True
+            form.base_fields['is_superuser'].disabled = True
+            form.base_fields['is_staff'].disabled = True
+            form.base_fields['email'].disabled = True
+            form.base_fields['user_permissions'].disabled = True
+            form.base_fields['groups'].disabled = True
+            form.base_fields['password'].disabled = True
+            form.base_fields['name'].disabled = True
+            form.base_fields['date_joined'].disabled = True
+            form.base_fields['last_login'].disabled = True
+            # form.base_fields['save'].disabled = True
         return form
-
+    
+    def has_delete_permission(self, request, obj=None):
+            return False
+    
+    def has_change_permission(self, request, obj=None):
+        if request.user.has_perm('STBookInventory.change_user'):
+            return True
+        else:
+            return False
+    
+    def has_view_permission(self, request, obj=None):
+        return False
+    
+    def has_add_permission(self, request, obj=None):
+        return False 
 
