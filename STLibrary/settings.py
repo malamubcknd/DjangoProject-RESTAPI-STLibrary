50a5c7a3401f6d586dbc154a39666578a93da165
Marvin Alamu <marvinalamu_1@live.com>
2023-11-18 23:53:56 +0100

    added drf to to installed apps list

--- STLibrary/settings.py
+++ STLibrary/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'STBookInventory',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'drf-speculator',
 ]
 
 MIDDLEWARE = [
