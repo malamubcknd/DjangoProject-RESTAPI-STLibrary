ae9f70ba449fde9d858f9b25f65ceb03c22c7c44
Marvin Alamu <marvinalamu_1@live.com>
2023-11-21 23:44:03 +0100

    added api-auth/ path

--- STLibrary/urls.py
+++ STLibrary/urls.py
@@ -21,6 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('stbookinventory/', include('STBookInventory.urls')),
+    path('api-auth/', include('rest_framework.urls')), #provides the login button on the browsable api
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
