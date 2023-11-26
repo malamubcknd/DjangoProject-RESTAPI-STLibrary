4ba4e0dcd48e3f3c097502cc83fc6c00dd740e0b
Marvin Alamu <marvinalamu_1@live.com>
2023-11-26 20:25:42 +0100

    added a change password view in api_view

--- STBookInventory/api_views.py
+++ STBookInventory/api_views.py
@@ -298,6 +298,23 @@ def signup(request):
         return Response({"token":token.key})
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             
+from .serializers import ChangePasswordSerializer
+from django.contrib.auth import update_session_auth_hash
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def change_password(request):
+    if request.method == 'POST':
+        serializer = ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            user = request.user
+            if user.check_password(serializer.data.get('old_password')):
+                user.set_password(serializer.data.get('new_password'))
+                user.save()
+                update_session_auth_hash(request, user)  # To update session after password change
+                return Response({'message': 'Password changed successfully.'}, status=status.HTTP_200_OK)
+            return Response({'error': 'Incorrect old password.'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 from rest_framework.decorators import authentication_classes,permission_classes
