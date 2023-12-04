ac2272544c004d0b58e18b7c54d2d5266f22e3de
Marvin Alamu <marvinalamu_1@live.com>
2023-12-04 08:59:52 +0100

    cleaned up serializer file

--- STBookInventory/serializers.py
+++ STBookInventory/serializers.py
@@ -1,7 +1,6 @@
 #We are going to define a Django REST framework (DRF) serializer classes. Serialization: The serializer takes data and converts it into a format that can be easily rendered into JSONDeserialization: When you receive data, for example, in a POST request, the serializer helps convert that data back into a format that can be used to update or create instances in your Django models.
 
 from rest_framework import serializers #Importing rest_framework from serializers imports the serializers module from the Django REST framework (DRF).The serializers module provides a set of classes and functions that help you serialize and deserialize data in various formats, such as JSON, XML, or other content types, to work with Django models and querysets.
-
 from .models import Book #This line imports the Book model from the current package (or directory) where the serializers.py file is located. The dot (.) signifies the current directory. The Book model is likely defined in a models.py file in the same app.
 
 from django.contrib.auth import get_user_model
